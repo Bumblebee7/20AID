a18bba27abc781bc11b6ece919992ac88603b2b7
grumpy-cat <grumpy-cat@mac>
2021-01-22 15:37:01 +0100

    Mock prototype

--- 20aid/src/App.js
+++ 20aid/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Bubble from "./Bubble";
+import { AiOutlineInfoCircle, AiFillGithub } from "react-icons/ai";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+        <h1 className="primary-color">20AID</h1>
       </header>
+      <main>
+        <Bubble />
+      </main>
+      <footer>
+        <AiOutlineInfoCircle />
+        <AiFillGithub />
+      </footer>
     </div>
   );
 }
---
+++ 20aid/src/Bubble.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Bubble() {
+  return (
+    <div id="circle">
+      <p className="primary-color">18:30</p>
+    </div>
+  );
+}
+
+export default Bubble;
